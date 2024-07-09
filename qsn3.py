be7423d4471291dcec6bf43b932228fca4262de2
Soumodeep Das <higherdas09@gmail.com>
2024-07-09 12:28:09 +0530

    Add files via upload

---
+++ qsn3.py
@@ -0,0 +1,20 @@
+
+if __name__=='__main__':
+    try:#Firstpart of qsn (no3)
+        num=int(input("Give your number to calculate Multiplication table\n"))
+        list=[num*i for i in range(1,11)]
+        print(list)
+        #secondpart of qsn (no5)
+        f = open("tables.txt", "a")
+        f.write(str(list))
+        f.close()
+    except ValueError:
+        print("\nInvalid input\n")
+        exit()
+    else:
+        print("\nMultiplication table is printed on text file\n")
+    finally:
+        print("\nThank you for using this program\n")
+
+
+    
